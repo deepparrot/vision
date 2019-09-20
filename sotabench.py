48991db771680e43b659e29eb9978c0030018878
deepparrot <53057954+deepparrot@users.noreply.github.com>
2019-09-20 15:09:19 +0100

    I believe!

--- sotabench.py
+++ sotabench.py
@@ -10,6 +10,14 @@ from sotabench_transforms import Normalize, Compose, Resize, ToTensor
 
 MODEL_NAME = 'fcn_resnet101'
 
+def cat_list(images, fill_value=0):
+    max_size = tuple(max(s) for s in zip(*[img.shape for img in images]))
+    batch_shape = (len(images),) + max_size
+    batched_imgs = images[0].new(*batch_shape).fill_(fill_value)
+    for img, pad_img in zip(images, batched_imgs):
+        pad_img[..., : img.shape[-2], : img.shape[-1]].copy_(img)
+    return batched_imgs
+
 def collate_fn(batch):
     images, targets = list(zip(*batch))
     batched_imgs = cat_list(images, fill_value=0)
